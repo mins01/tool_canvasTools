629b8c49c4474b72668a533ee59a9110c5ce7c0f
mins01 <mins01.lycos.co.kr@gmail.com>
2022-11-23 11:30:13 +0900

    first

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tool_canvasTools
-tool_canvasTools
+html canvas 활용을 위한 툴(함수) 모음
---
+++ canvasTools.js
@@ -0,0 +1,198 @@
+const canvasTools = {
+  toImageElementDelay:0, //toXXX 의 딜레이 부분
+  textBox(ctxConf,width,height,text,lineHeightPx,paddingPx,bgFillStyle){
+    if(!ctxConf) ctxConf = {};
+    if(!bgFillStyle) bgFillStyle = null;
+    if(paddingPx==undefined) paddingPx = 0;
+    let canvas = document.createElement('canvas');
+    canvas.dataset.width = width
+    canvas.dataset.height = height
+    canvas.dataset.text = text
+    canvas.dataset.lineHeightPx = lineHeightPx
+    canvas.dataset.paddingPx = paddingPx
+    canvas.width = width;
+    canvas.height = height?height:300; //height 가 없으면 밑에서 자동 재계산한다.
+    let ctx = canvas.getContext('2d');
+    console.log();
+    for(var k in ctxConf){
+      ctx[k] = ctxConf[k];
+    }
+    // 줄바꿈 계산
+    let textWidth = canvas.width-paddingPx*2;
+    let lines = [''];
+    let linePos = 0;
+    let tmpText = '';
+    for(let i=0,m=text.length;i<m;i++){
+      
+      tmpText = lines[linePos];
+      tmpText += text[i];
+      if(text[i] == '\n'){ //줄바꿈 처리
+        lines.push(text[i].trim())
+        linePos++;
+        continue;
+      }
+      if(i===0 || textWidth >= ctx.measureText(tmpText).width){
+        lines[linePos] = tmpText;
+      }else{
+        lines[linePos] = lines[linePos].trim();
+        // lines[linePos] = lines[linePos];
+        lines.push(text[i])
+        linePos++
+      }
+    }
+
+
+    if(!height){
+      canvas.height = Math.ceil(lineHeightPx*(lines.length)) + (paddingPx*2);
+    }
+    ctx = canvas.getContext('2d');
+    if(bgFillStyle){
+      ctx.save();
+      ctx.fillStyle = bgFillStyle;
+      ctx.beginPath()
+      ctx.rect(0,0,canvas.width,canvas.height);
+      ctx.fill()
+      ctx.restore();
+    }
+    for(var k in ctxConf){
+      ctx[k] = ctxConf[k];
+    }
+
+    let x = 0;
+    switch(ctx.textAlign){
+      case 'center':x=Math.ceil(canvas.width/2); break;
+      case 'right':x=canvas.width-paddingPx; break;
+      case 'left':x=paddingPx; break;
+    }
+    lines.forEach((text,idx)=>{
+      console.log('y',lineHeightPx*(idx+1)+paddingPx);
+      ctx.fillText(text, x, lineHeightPx*(idx+1)+paddingPx)
+      ctx.strokeText(text, x, lineHeightPx*(idx+1)+paddingPx)
+    })
+
+    
+    return canvas
+  },
+  toCanvas(canvas,bgFillStyle){ // clone
+    return this.fromImage(canvas,bgFillStyle)
+  },
+  fromImage(image,bgFillStyle){
+    let canvas = document.createElement('canvas');
+    canvas.width = image.naturalWidth || image.width;
+    canvas.height = image.naturalHeight || image.height;
+    let ctx = canvas.getContext('2d')
+    if(bgFillStyle){
+      ctx.save();
+      ctx.fillStyle = bgFillStyle;
+      ctx.beginPath()
+      ctx.rect(0,0,canvas.width,canvas.height);
+      ctx.fill()
+      ctx.restore();
+    }
+    ctx.drawImage(image,0,0,canvas.width,canvas.height,0,0,canvas.width,canvas.height);
+    return canvas;
+  },
+  merge(target,source,sx,sy,sw,sh,dx,dy,dw,dh,rotateCenterDegree){
+    let ctx = target.getContext('2d')
+    const rotateAngle = rotateCenterDegree *  Math.PI / 180;
+    ctx.save();
+    ctx.translate(dx+dw/2, dy+dh/2);
+    ctx.rotate(rotateAngle);
+    ctx.translate(-1*(dx+dw/2), -1*(dy+dh/2));
+    // ctx.translate(dx, dy);
+    // ctx.drawImage(source,sx,sy,sw,sh,0,0,dw,dh);
+    ctx.drawImage(source,sx,sy,sw,sh,dx,dy,dw,dh);
+    ctx.restore();
+  },
+  
+  toBlob(canvas,cb,type,encoderOptions){
+    canvas.toBlob((blob) => {
+      cb(blob)
+    },type,encoderOptions);
+  },
+  toDataURL(canvas,cb,type,encoderOptions){
+    const dataURL =canvas.toDataURL(type,encoderOptions);
+    cb(dataURL)
+  },
+  toImage(canvas,cb,type,encoderOptions){
+    try{
+      // throw 'error test'; //for debug
+      return this.toImageWithBlob(canvas,cb,type,encoderOptions);
+    }catch(e){
+      console.error(e)
+      return this.toImageWithDataURL(canvas,cb,type,encoderOptions);
+    }
+  },
+  toImageWithBlob(canvas,cb,type,encoderOptions){
+    if(!type){type='image/png'}
+    let img = new Image();
+    img.crossOrigin="anonymous"
+    img.dataset.type=type;
+    img.dataset.encoderOptions=encoderOptions;
+    this.toBlob(canvas,(blob) => {
+      img.dataset.type=blob.type;
+      img.dataset.size=blob.size;
+      const url = URL.createObjectURL(blob);
+      img.onload = (event)=>{
+        // console.log('img.onload');
+        URL.revokeObjectURL(url);
+        if(cb){
+          if(this.toImageElementDelay>0){ setTimeout(()=>{ cb(img); },this.toImageElementDelay) }
+          else{ cb(img); }
+        }
+      }
+      img.src = url;
+    },type,encoderOptions);
+  },
+  toImageWithDataURL(canvas,cb,type,encoderOptions){
+    if(!type){type='image/png'}
+    let img = new Image();
+    img.crossOrigin="anonymous"
+    img.dataset.type=type;
+    img.dataset.encoderOptions=encoderOptions;
+    this.toDataURL(canvas,(dataURL) => {     
+      img.onload = (event)=>{
+        console.log('img.onload');
+        if(cb){
+          if(this.toImageElementDelay>0){ setTimeout(()=>{ cb(img); },this.toImageElementDelay) }
+          else{ cb(img); }
+        }
+      }
+      img.src = dataURL;
+    },type,encoderOptions);
+  },
+  downloadWithBlob(canvas,filename,type,encoderOptions){
+    this.toBlob(canvas,(blob) => {
+      const url = URL.createObjectURL(blob);
+      let a = document.createElement('a');
+      a.style.display = 'none';
+      document.body.appendChild(a);
+      a.download = filename;
+      a.href = url
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    },type,encoderOptions);
+  },
+  downloadWithDataURL(canvas,filename,type,encoderOptions){
+    this.toDataURL(canvas,(dataURL) => {
+      // const url = URL.createObjectURL(blob);
+      let a = document.createElement('a');
+      a.style.display = 'none';
+      document.body.appendChild(a);
+      a.download = filename;
+      a.href = dataURL
+      a.click();
+      // document.body.removeChild(a);
+    },type,encoderOptions);
+  },
+  download(canvas,filename,type,encoderOptions){
+    try{
+      // throw 'error test'; //for debug
+      return this.downloadWithBlob(canvas,filename,type,encoderOptions);
+    }catch(e){
+      console.error(e)
+      return this.downloadWithDataURL(canvas,filename,type,encoderOptions);
+    }
+  }
+}
